9be8b5b2e801e33901e2f4a1076c478d0dd16707
SerhiiSamara <104717475+SerhiiSamara@users.noreply.github.com>
2022-07-12 01:50:54 +0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -2,13 +2,17 @@
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+		modal: document.querySelector("[data-modal]"),
+		window: document.querySelector("[data-window]"),
+		body: document.querySelector('body'),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+		refs.modal.classList.toggle("is-hidden");
+		refs.window.classList.toggle("is-scroll");
+		refs.body.classList.toggle('no-scroll');
   }
 })();
